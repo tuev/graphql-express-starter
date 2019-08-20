5e2d5ae00f85b7fd370d75423d6d46b23daa1fe4
tue <tuevu0171@gmail.com>
2019-08-20 23:07:40 +0700

    feat: update product resolver

--- src/modules/Product/product.resolver.js
+++ src/modules/Product/product.resolver.js
@@ -1,11 +1,12 @@
 import Product from './product.model'
 import { pick, get } from 'lodash'
+import { subscriptionCreator } from '@utils'
+import * as productConst from './product.constant'
 
 /* ------------------------------- QUERY ------------------------------- */
 
 const products = async () => {
   const products = await Product.find({})
-  console.log(products, 'products')
   return products
 }
 
@@ -16,31 +17,61 @@ const product = async (_, { id }) => {
 
 /* ----------------------------- MUTATION ---------------------------- */
 
-const addProduct = async (_, args) => {
-  const newProduct = pick(args.input, [
+const addProduct = async (_, args = {}, { pubsub } = {}) => {
+  const productInfo = pick(args.input, [
     'name',
-    'slug',
     'description',
     'isPublic',
     'status',
     'releaseDate'
   ])
+  const productRelation = pick(args, ['SKUs'])
+  const slug = args.slug || productInfo.name
   const product = await Product.create({
-    ...newProduct,
-    slug: newProduct.slug || newProduct.name,
-    releaseDate: newProduct.releaseDate || new Date()
+    ...productInfo,
+    ...productRelation,
+    slug,
+    releaseDate: productInfo.releaseDate || new Date()
   })
+  pubsub.publish(productConst.PRODUCT_ADDED, product)
   return product
 }
 
-const deleteProduct = async (_, { id }) => {
+const updateProduct = async (_, args = {}, { pubsub } = {}) => {
+  const productInfo = pick(args.input, [
+    'name',
+    'description',
+    'isPublic',
+    'status',
+    'releaseDate'
+  ])
+  const productRelation = pick(args, ['SKUs'])
+  const product = await Product.findByIdAndUpdate(
+    args.id,
+    {
+      ...productInfo,
+      ...productRelation,
+      releaseDate: productInfo.releaseDate || new Date()
+    },
+    { new: true }
+  )
+  pubsub.publish(productConst.PRODUCT_UPDATED, product)
+  return product
+}
+
+const deleteProduct = async (_, { id }, { pubsub } = {}) => {
   const result = await Product.deleteOne({ _id: id })
+  pubsub.publish(productConst.PRODUCT_DELETED, id)
   return !!get(result, 'deletedCount', false)
 }
 
 /* ---------------------------- APPLY MIDDLEWARE ---------------------------- */
 
-/* ------------------------------ SUBCRIBE ----------------------------- */
+/* -------------------------------- SUBCRIBE -------------------------------- */
+
+const productAdded = subscriptionCreator({ name: productConst.PRODUCT_ADDED })
+const productUpdated = subscriptionCreator({ name: productConst.PRODUCT_UPDATED })
+const productDeleted = subscriptionCreator({ name: productConst.PRODUCT_DELETED })
 
 /* -------------------------------------------------------------------------- */
 /*                                   EXPORT                                   */
@@ -48,6 +79,6 @@ const deleteProduct = async (_, { id }) => {
 
 export const productResolvers = {
   Query: { products, product },
-  Mutation: { addProduct, deleteProduct },
-  Subscription: {}
+  Mutation: { addProduct, deleteProduct, updateProduct },
+  Subscription: { productAdded, productUpdated, productDeleted }
 }
