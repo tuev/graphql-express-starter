82b7b25fbeeb4105fcd26cf8a40d7ede66f8b290
tue <tuevu0171@gmail.com>
2019-08-20 23:07:26 +0700

    feat: update image resolver

--- src/modules/Image/image.resolver.js
+++ src/modules/Image/image.resolver.js
@@ -1,5 +1,7 @@
 import Image from './image.model'
 import { pick, get } from 'lodash'
+import { subscriptionCreator } from '@utils'
+import * as imageConst from './image.constant'
 
 /* ------------------------------- QUERY ------------------------------- */
 
@@ -15,23 +17,44 @@ const image = async (_, { id }) => {
 
 /* ----------------------------- MUTATION ---------------------------- */
 
-const addImage = async (_, args = {}) => {
-  const newImage = pick(args.input, ['name', 'url', 'slug', 'description'])
+const addImage = async (_, args = {}, { pubsub }) => {
+  const newImage = pick(args.input, ['name', 'url', 'description'])
   const image = await Image.create({
     ...newImage,
-    slug: newImage.slug || newImage.name
+    slug: args.slug || newImage.name
   })
+
+  pubsub.publish(imageConst.IMAGE_ADDED, image)
   return image
 }
 
-const deleteImage = async (_, { id }) => {
+const updateImage = async (_, args = {}, { pubsub }) => {
+  const newImage = pick(args.input, ['name', 'url', 'description'])
+  const image = await Image.create(
+    args.id,
+    {
+      ...newImage
+    },
+    { new: true }
+  )
+
+  pubsub.publish(imageConst.IMAGE_UPDATED, image)
+  return image
+}
+
+const deleteImage = async (_, { id }, { pubsub } = {}) => {
   const result = await Image.deleteOne({ _id: id })
+  pubsub.publish(imageConst.IMAGE_DELETED, id)
   return !!get(result, 'deletedCount', false)
 }
 
 /* ---------------------------- APPLY MIDDLEWARE ---------------------------- */
 
-/* ------------------------------ SUBCRIBE ----------------------------- */
+/* -------------------------------- SUBCRIBE -------------------------------- */
+
+const imageAdded = subscriptionCreator({ name: imageConst.IMAGE_ADDED })
+const imageUpdated = subscriptionCreator({ name: imageConst.IMAGE_UPDATED })
+const imageDeleted = subscriptionCreator({ name: imageConst.IMAGE_DELETED })
 
 /* -------------------------------------------------------------------------- */
 /*                                   EXPORT                                  */
@@ -39,6 +62,6 @@ const deleteImage = async (_, { id }) => {
 
 export const imageResolvers = {
   Query: { images, image },
-  Mutation: { addImage, deleteImage },
-  Subscription: {}
+  Mutation: { addImage, deleteImage, updateImage },
+  Subscription: { imageAdded, imageUpdated, imageDeleted }
 }
