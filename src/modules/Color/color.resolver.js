255eddcf08ed00c47b7cf15b2eb5f70634acb8e6
tue <tuevu0171@gmail.com>
2019-08-20 23:07:12 +0700

    feat: update color resolver

--- src/modules/Color/color.resolver.js
+++ src/modules/Color/color.resolver.js
@@ -1,5 +1,8 @@
 import Color from './color.model'
 import { pick, get } from 'lodash'
+import { subscriptionCreator } from '@utils'
+import * as colorConst from './color.constant'
+
 /* ------------------------------- QUERY ------------------------------- */
 
 const colors = async () => {
@@ -14,32 +17,55 @@ const color = async (_, { id }) => {
 
 /* ----------------------------- MUTATION ---------------------------- */
 
-const addColor = async (_, args = {}) => {
-  const newColor = pick(args.input, ['name', 'slug', 'value', 'description'])
+const addColor = async (_, args = {}, { pubsub } = {}) => {
+  const colorInfo = pick(args.input, ['name', 'value', 'description'])
+  const colorRelation = pick(args, ['SKUs'])
   const color = await Color.create({
-    ...newColor,
-    value: newColor.value || 'white',
-    slug: newColor.slug || newColor.name
+    ...colorInfo,
+    ...colorRelation,
+    slug: args.slug || colorInfo.name
   })
+  pubsub.publish(colorConst.COLOR_ADDED, color)
   return color
 }
 
-const deleteColor = async (_, { id }) => {
+const updateColor = async (_, args = {}, { pubsub } = {}) => {
+  const colorInfo = pick(args.input, ['name', 'value', 'description'])
+  const id = get(args, 'id', '')
+  const colorRelation = pick(args, ['SKUs'])
+  const color = await Color.findByIdAndUpdate(
+    id,
+    {
+      ...colorInfo,
+      ...colorRelation
+    },
+    { new: true }
+  )
+  pubsub.publish(colorConst.COLOR_UPDATED, color)
+  return color
+}
+
+const deleteColor = async (_, { id }, { pubsub } = {}) => {
   const result = await Color.deleteOne({ _id: id })
   const deletedColor = get(result, 'deletedCount', false)
+  pubsub.publish(colorConst.COLOR_DELETED, id)
   return !!deletedColor
 }
 
 /* ---------------------------- APPLY MIDDLEWARE ---------------------------- */
 
-/* ------------------------------ SUBCRIBE ----------------------------- */
+/* -------------------------------- SUBCRIBE -------------------------------- */
+
+const colorAdded = subscriptionCreator({ name: colorConst.COLOR_ADDED })
+const colorUpdated = subscriptionCreator({ name: colorConst.COLOR_UPDATED })
+const colorDeleted = subscriptionCreator({ name: colorConst.COLOR_DELETED })
 
 /* -------------------------------------------------------------------------- */
-/*                                   EXPORT                                   */
+/*                                   EXPORT                                  */
 /* -------------------------------------------------------------------------- */
 
 export const colorResolvers = {
   Query: { colors, color },
-  Mutation: { addColor, deleteColor },
-  Subscription: {}
+  Mutation: { addColor, deleteColor, updateColor },
+  Subscription: { colorAdded, colorUpdated, colorDeleted }
 }
